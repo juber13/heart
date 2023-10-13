f125095179bb16ff9a3492010afc894e7a0e1974
juber <juberk1010@gmail.com>
2023-10-14 00:17:17 +0530

    make another animation

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const arr = ["red" , "blue" , "black" , "green" , "pink"]
 function createHeart(){
     const heart = document.createElement('div');
     heart.className = "heart"
-    heart.innerHTML  = '❤️';
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+    heart.style.backgroundColor = arr[Math.floor(Math.random() * arr.length)];
+    // heart.style.width = Math.random() * 1000 + 'px'
+    // heart.style.height = Math.random() * 100 + 'px'
     document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    },5000)
 }
 
-setInterval(createHeart , 1000)
+setInterval(createHeart , 100)
